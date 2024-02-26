192853e1a888f4e2c4ee48db63666478d0886098
Ramkumar-2269 <ramkumarakula84@gmail.com>
2024-02-26 17:46:29 +0530

    projects created

---
+++ Backend/foodexpress/src/main/java/com/example/foodexpress/FoodexpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodexpressApplication.class, args);
+	}
+
+}
---
+++ Backend/foodexpress/src/test/java/com/example/foodexpress/FoodexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
