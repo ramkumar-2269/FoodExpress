8a532cb08cb9d53c82ef6e7b5a0bc0595d405816
Ramkumar-2269 <ramkumarakula84@gmail.com>
2024-03-03 11:27:13 +0530

    app.js merge conflicts resolved

--- Frontend1/reactapp/src/App.js
+++ Frontend1/reactapp/src/App.js
@@ -4,30 +4,12 @@ import Home from './Components/Home';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
         </Routes>
       </BrowserRouter>
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> new project 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> eb6143bcb89b1b03521619306b496a8f05a2483f
     </div>
   )
 }
